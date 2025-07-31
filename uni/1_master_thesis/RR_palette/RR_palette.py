65f270eacd017be77d29a2e1c7ad7b60400d8a64
Marco Bella <bellamarco01@gmail.com>
2025-07-31 14:41:54 +0200

    added palette

---
+++ uni/1_master_thesis/RR_palette/RR_palette.py
@@ -0,0 +1,18 @@
+palette = [
+    '#ad7577',  # Old rose
+    '#b9c1f1',  # Periwinkle
+    '#a09bb2',  # Rose quartz
+    '#776158',  # Wenge
+    '#9c5429',  # Brown
+    '#1e1b08',  # Smoky black
+]
+
+named_palette = {
+    'Old rose': '#ad7577',
+    'Periwinkle': '#b9c1f1',
+    'Rose quartz': '#a09bb2',
+    'Wenge': '#776158',
+    'Brown': '#9c5429',
+    'Smoky black': '#1e1b08'
+}
+
